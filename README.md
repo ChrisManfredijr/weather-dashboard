75fb081f7c4bceaca36bdcb849476ea8bf9bc087
Chris <chrismanfredijr@gmail.com>
2022-05-04 18:10:35 -0400

    final changes

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # weather-dashboard
+
+![weather dasboard](./assets/Capture.PNG)
+
+## purpose
+to create an interactive weather dashboard to look up the current weather and a 5 day forecast. Uses Weather One API to get weather data by city.
+
+## built with 
+HTML, CSS, JS, Jquery, Bootstrap, Weather One API
+
+## built by 
+Chris Manfredi
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
 var key = "8b316af2cd911d56c695445d7a5a016a";
 var cityName;
-var listIndex = 0;
+
 
 $("#searchForm").on("click", "#searchCity", function(event) {
     event.preventDefault();
     var cityName = $('input[name=city]').val();
     var cityName = cityName.trim();
     geoGet(cityName);
-})
+});
 
+$("#searchForm").on("click", ".prevCity", function(event) {
+    event.preventDefault();
+    var cityButton = $(this).attr("value");
+    geoGet(cityButton);
+});
 //gets the longitude and latitute on start
 var geoGet = function(cityName){
     var geoAPI = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + key;
@@ -19,6 +24,7 @@ var geoGet = function(cityName){
         var lon = data[0].lon;
         weatherReport(lat, lon);
         fiveDayReport(lat, lon);
+        cityLog(cityName);
       });
     } else {
       document.location.replace("./index.html");
@@ -27,13 +33,66 @@ var geoGet = function(cityName){
    
 };
 
+geoGet("New York");
+
+
+var cityLog = function(cityName, lat, lon) {
+    if(localStorage.getItem("cities") === null){
+        localStorage.setItem("cities", "[]")
+    }
+    var cityLogged = {
+        city: cityName,
+        lat: lat,
+        lon: lon,
+    }
+    
+    var tempStorage = JSON.parse(localStorage.getItem('cities'));
+  
+    for(let i = 0; i < tempStorage.length; i++){
+        if(tempStorage[i].lat === cityLogged.lat && tempStorage[i].lon === cityLogged.lon){
+            break;
+            
+        }else if(i === tempStorage.length - 1){
+            tempStorage.push(cityLogged);
+            localStorage.setItem("cities", JSON.stringify(tempStorage));
+            var city = cityLogged.city;
+            var cityButton = $((document.createElement('input')));
+            cityButton.attr("type", "submit");
+            cityButton.attr("value", city); 
+            cityButton.addClass("prevCity w-100 m-1");
+            $("#searchField").append(cityButton);
+        }
+        
+    }
+}
+
+var localStorageButtons = function(){
+    var tempStorage = JSON.parse(localStorage.getItem('cities'));
+    for(var i = 0; i < tempStorage.length; i++){
+        
+        var cityButton = $((document.createElement('input')));
+        var city = tempStorage[i].city;
+        cityButton.attr("type", "submit");
+        cityButton.attr("value", city); 
+        cityButton.addClass("prevCity w-100 m-1");
+        $("#searchField").append(cityButton);
+       
+        
+    }
+}
+
+localStorageButtons();
+
 //current weather report
 var weatherReport = function(lat, lon) {
     var weatherAPI = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&units=imperial";
+    
+    
+    
     fetch(weatherAPI).then(function(response){ 
         if (response.ok) {
             response.json().then(function(data) {
-            weatherPopulate(data);
+            weatherPopulate(data, lat, lon);
           });
         } else {
           document.location.replace("./index.html");
@@ -42,8 +101,7 @@ var weatherReport = function(lat, lon) {
 };
 
 var weatherPopulate = function(data) {
-    console.log(data);
-    console.log(data.weather[0].icon);
+   
     let cityInfo = {
         city:(data.name),
         icon: "http://openweathermap.org/img/wn/" + (data.weather[0].icon) + "@2x.png",
@@ -66,7 +124,6 @@ var fiveDayReport = function(lat, lon) {
     if (response.ok) {
         response.json().then(function(data) {
         weatherPopulateFive(data);
-        console.log(data);
       });
     } else {
       document.location.replace("./index.html");
